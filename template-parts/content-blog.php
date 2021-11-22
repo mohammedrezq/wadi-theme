b78cd99968c9a23bfeacb691f3335448e9267303
mohammedrezq <mohammedrezq2000@gmail.com>
2021-11-22 20:27:02 +0200

    [Wadi-2] Added pagination to blog Page

--- template-parts/content-blog.php
+++ template-parts/content-blog.php
@@ -8,10 +8,12 @@
  */
 
 
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
  $args = array(
     'post_status' => 'publish',
     'post_type' => 'post',
-    'posts_per_page' => -1,
+    'posts_per_page' => 4,
+    'paged' => $paged,
     'ignore_sticky_posts' => 1
  );
 
@@ -25,6 +27,7 @@ $the_query = new WP_Query($args);
 
     <div class="blog__content-posts">
     <?php
+
     if ($the_query->have_posts()) {
         while ($the_query->have_posts()) {
             $the_query->the_post(); ?>
@@ -53,11 +56,31 @@ $the_query = new WP_Query($args);
                 </div>
         </div>
             <?php
-        }
+        } ?>
+        
+    </div>
+        <div class="wadi-pagination">
+    <?php
+
+        echo paginate_links(array(
+            'base'         => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
+            'total'        => $the_query->max_num_pages,
+            'current'      => max(1, get_query_var('paged')),
+            'format'       => '?paged=%#%',
+            'show_all'     => false,
+            'type'         => 'plain',
+            'end_size'     => 2,
+            'mid_size'     => 1,
+            'prev_next'    => true,
+            'prev_text'    => sprintf('<i></i> %1$s', __('« Previous Page', 'wadi')),
+            'next_text'    => sprintf('%1$s <i></i>', __('Next Page »', 'wadi')),
+            'add_args'     => false,
+            'add_fragment' => '',
+        )); ?>
+</div>
+<?php
     }
     /* Restore original Post Data */
     wp_reset_postdata();
     ?>
-        
-    </div>
 </div>
