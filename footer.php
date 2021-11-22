d0f41309c504d4a90bdaafaca87273993bdb8215
mohammedrezq <mohammedrezq2000@gmail.com>
2021-11-22 17:47:13 +0200

    [Wadi-2] Added Footer 2 Dynamic Widget (recent posts, and categories) and two static widgets site info and Contact

--- footer.php
+++ footer.php
@@ -12,19 +12,70 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wadi' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'wadi' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wadi' ), 'wadi', '<a href="https://www.wadiweb.com">Mohammed Rezq</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="footer-wrap">
+			<?php
+            /**
+             * Wadi Site info Footer
+             */
+            ?>
+			<div class="footer-section">
+				<div class="wadi__site-info">
+					<div class="wadi__site-url">
+						<a href="<?php echo site_url(); ?>"><?php echo get_bloginfo('name'); ?></a>
+					</div>
+					<div class="wadi__search-footer">
+						<?php
+                            echo get_search_form()
+                        ?>
+					</div>
+				</div>
+			</div>
+			<?php
+            /**
+             * Recent Posts Widget
+             */
+            ?>
+			<div class="footer-section">
+            	<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('wadi-footer-column-one-widget')) {
+                ;
+            } ?>
+        	</div>
+			<?php
+            /**
+             * Topic Widget (Categories)
+             */
+            ?>
+        	<div class="footer-section">
+        	    <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('wadi-footer-column-two-widget')) {
+                ;
+            } ?>
+        	</div>
+			<?php
+            /**
+             * Wadi Contact Footer
+             */
+            ?>
+			<div class="footer-section">
+			<div class="wadi__footer-contact">
+				<h4 class="wadi__contact-head">
+					Contact
+				</h4>
+				<ul class="wadi__contact-list">
+					<li>
+					<i class="fas fa-envelope"></i><a href="https://www.wadiweb.com/contact">Email</a>
+					</li>
+					<li>
+					<i class="fab fa-facebook-f"></i><a href="https://www.facebook.com/WadiWeb">Facebook</a>
+					</li>
+					<li>
+					<i class="fab fa-twitter"></i><a href="https://www.twitter.com/WadiWeb">Twitter</a>
+					</li>
+				</ul>
+			</div>	
+			</div>
+
+
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
