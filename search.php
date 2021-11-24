3cedbfa92cdf6fdf8f211a76cf3e48278fa1b2d7
mohammedrezq <mohammedrezq2000@gmail.com>
2021-11-24 17:12:43 +0200

    [Wadi-6] Added Pagination to Search Page and Fixed responsivity for Search Page!

--- search.php
+++ search.php
@@ -42,12 +42,31 @@ get_header();
 				get_template_part( 'template-parts/content', 'search' );
 
 			endwhile;
-
-			the_posts_navigation();
+			
 			?>
-
-
-			</div>
+		</div>
+		<?php
+		
+					$big = 999999999; // need an unlikely integer
+					echo '<div class="wadi-pagination">';
+						echo paginate_links(array(
+						'base'         => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+						'format'       => '?paged=%#%',
+						'total'        => $wp_query->max_num_pages,
+						'current'      => max(1, get_query_var('paged')),
+						'show_all'     => false,
+						'type'         => 'plain',
+						'end_size'     => 2,
+						'mid_size'     => 1,
+						'prev_next'    => true,
+						'prev_text'    => sprintf('<i></i> %1$s', __('« Previous Page', 'wadi')),
+						'next_text'    => sprintf('%1$s <i></i>', __('Next Page »', 'wadi')),
+						'add_args'     => false,
+						'add_fragment' => '',
+						));
+					echo '</div>';
+		
+					?>
 			</div>
 			<?php
 		else :
